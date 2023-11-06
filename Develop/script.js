9aae0b089d6aa59bbf722643cfcafd0fc399e60c
Ryan <ryanmckfiddle@gmail.com>
2023-11-06 15:18:27 -0800

    Added in js var list

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,29 @@
 // Assignment code here
+// var list
 
+var charaters;
 
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var lowercaseletters = [ 'a', 'b', 'c' , 'd' , 'e', 'f', 'g', 'h' , 
+'i', 'j' , 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+'w', 'x', 'y', 'z']
+
+var uppercaseletters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+ 'W', 'X', 'Y', 'Z']
+
+var specialcharaters = [ '!', '@', '#', '$', '%', '^',
+ '&', '*', '(', ')', '~']
+
+ 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
