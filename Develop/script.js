95d3452003f676046b01ffde102977c17c43d879
Ryan <ryanmckfiddle@gmail.com>
2023-11-06 16:28:18 -0800

    Added Prompt function with character parameters using an empty character array that will return the users imputed number and 'if' prompts into the empty character array using .concat to merg the previously defined character arrays to meet the users selected prompts.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,8 @@
 // Assignment code here
 // var list
 
-var charaters;
+
+var characterLength = 8;
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -13,10 +14,19 @@ var uppercaseletters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
  'W', 'X', 'Y', 'Z']
 
-var specialcharaters = [ '!', '@', '#', '$', '%', '^',
+var specialcharacters = [ '!', '@', '#', '$', '%', '^',
  '&', '*', '(', ')', '~']
 
  
+ // password function 
+
+ function makePassword() {
+  var password = gereratePassword();
+
+
+
+ }
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -25,7 +35,16 @@ var generateBtn = document.querySelector("#generate");
 
 
 
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 // Write password to the #password input
+
+
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -34,5 +53,39 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// Adding characters prompts for the user
+
+var characters = [];
+
+function Prompts() {
+
+characterLength = parseInt(prompt("Select password length between 8 and 128 characters."));
+
+
+
+if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+  alert("Character length parameters (8-128 characters) not met.");
+  return false;
+}
+
+
+if (confirm("Incorporate numbers in password?")) {
+  characters = characters.concat(numbers);
+}
+
+if (confirm("Incorporate lower case letters in password?")) {
+  characters = characters.concat(lowercaseletters);
+}
+
+
+if (confirm("Incorporate upper case letters in password?")) {
+  characters = characters.concat(uppercaseletters);
+}
+
+if (confirm("Incorporate special characters in password?")) {
+  characters = characters.concat(specialcharacters);
+}
+return true;
+
+}
